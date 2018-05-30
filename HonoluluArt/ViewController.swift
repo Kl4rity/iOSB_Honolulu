52d27e9d0112fb3a62e64fdbee6dc0a493ea43d8
Clemens Stift <clemens@stift.online>
2018-05-30 12:10:17 +0200

    Implemented annotation view and set mapView delegate to ViewController.swift

--- HonoluluArt/ViewController.swift
+++ HonoluluArt/ViewController.swift
@@ -42,6 +42,8 @@ class ViewController: UIViewController {
         let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
         centerMapOnLocation(location: initialLocation)
         
+        mapView.delegate = self
+        
         let artwork = Artwork(title: "King David Kalakaua",
                               locationName: "Waikiki Gateway Park",
                               discipline: "Sculpture",
@@ -60,7 +62,21 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: MKMapViewDelegate {
-    
-    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let annotation = annotation as? Artwork else { return nil }
+        let identifier = "marker"
+        var view: MKMarkerAnnotationView
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+            as? MKMarkerAnnotationView {
+            dequeuedView.annotation = annotation
+            view = dequeuedView
+        } else {
+            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.canShowCallout = true
+            view.calloutOffset = CGPoint(x: -5, y: 5)
+            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        return view
+    }
 }
 
