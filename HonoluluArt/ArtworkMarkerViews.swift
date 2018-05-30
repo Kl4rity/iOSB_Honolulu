71e7d29799da76f861979a5ad201584bb4020ff1
Clemens Stift <clemens@stift.online>
2018-05-30 14:18:34 +0200

    minor change

---
+++ HonoluluArt/ArtworkMarkerViews.swift
@@ -0,0 +1,45 @@
+//
+//  ArtworkViews.swift
+//  HonoluluArt
+//
+//  Created by Clemens Stift on 30.05.18.
+//  Copyright © 2018 Ray Wenderlich. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class ArtworkMarkerView: MKMarkerAnnotationView {
+    override var annotation: MKAnnotation? {
+        willSet {
+            guard let artwork = newValue as? Artwork else { return }
+            canShowCallout = true
+            calloutOffset = CGPoint(x: -5, y: 5)
+            rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            markerTintColor = artwork.markerTintColor
+//            glyphText = String(artwork.discipline.first!)
+            if let imageName = artwork.imageName {
+                glyphImage = UIImage(named: imageName)
+            } else {
+                glyphImage = nil
+            }
+        }
+    }
+}
+
+class ArtworkView: MKAnnotationView {
+    override var annotation: MKAnnotation? {
+        willSet {
+            guard let artwork = newValue as? Artwork else {return}
+            canShowCallout = true
+            calloutOffset = CGPoint(x: -5, y: 5)
+            rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            
+            if let imageName = artwork.imageName {
+                image = UIImage(named: imageName)
+            } else {
+                image = nil
+            }
+        }
+    }
+}
